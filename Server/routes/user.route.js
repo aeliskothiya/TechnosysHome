472a068c35ca0b9ac8f4a2e79c77c1a59b4ec39a
aeliskothiya <22bmiit009@gmail.com>
2025-11-27 10:28:25 +0530

    fix

--- Server/routes/user.route.js
+++ Server/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import userAuth from '../middleware/userAuth.js';
-import { getTechnicianWallet,getUserData } from '../controllers/user.Controller.js';
+import { getTechnicianWallet,getUserData } from '../controllers/user.controller.js';
 
 
 const userRoutesr = express.Router();
