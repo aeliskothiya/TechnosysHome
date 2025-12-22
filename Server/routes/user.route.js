40783a554817bc7225f393f705fd41f8afcbd511
aeliskothiya <22bmiit009@gmail.com>
2025-12-22 18:53:48 +0530

    gggggggggggg

--- Server/routes/user.route.js
+++ Server/routes/user.route.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import userAuth from '../middleware/userAuth.js';
-import { getUserData } from '../controllers/user.Controller.js';
-import { getTechnicianWallet } from '../controllers/user.Controller.js';
+import { getUserData } from '../controllers/user.controller.js';
+import { getTechnicianWallet } from '../controllers/user.controller.js';
 
 
 const userRoutesr = express.Router();
